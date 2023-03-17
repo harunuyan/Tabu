a0e31eb5888cebab2ab4cbb2f9973b17290246bf
monster <harunuyan6@gmail.com>
2023-03-17 16:38:31 +0300

    implemented hilt in project

---
+++ app/src/main/java/com/volie/tabu/di/NetworkModule.kt
@@ -0,0 +1,9 @@
+package com.volie.tabu.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule
--- app/src/main/java/com/volie/tabu/ui/MainActivity.kt
+++ app/src/main/java/com/volie/tabu/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.volie.tabu
+package com.volie.tabu.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.volie.tabu.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
