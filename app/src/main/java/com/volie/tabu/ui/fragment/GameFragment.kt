8595747d5205a237dedce1a6d5afb9e5a6ef0a79
monster <harunuyan6@gmail.com>
2023-03-19 12:50:07 +0300

    implemented fragments

---
+++ app/src/main/java/com/volie/tabu/ui/fragment/GameFragment.kt
@@ -0,0 +1,27 @@
+package com.volie.tabu.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.tabu.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    private var _mBinding: FragmentGameBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentGameBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _mBinding = null
+    }
+}
---
+++ app/src/main/java/com/volie/tabu/ui/fragment/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.volie.tabu.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.tabu.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private var _mBinding: FragmentSettingsBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _mBinding = null
+    }
+}
