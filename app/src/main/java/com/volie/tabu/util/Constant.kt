7aff8c0850365075d4c926ad87733ea8428ba70c
monster <harunuyan6@gmail.com>
2023-03-19 12:42:14 +0300

    implemented Constant; BASE_URL, CONTENT_API

---
+++ app/src/main/java/com/volie/tabu/util/Constant.kt
@@ -0,0 +1,7 @@
+package com.volie.tabu.util
+
+object Constant {
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+    const val CONTENT_API = "harunuyan/TabuApi/main/TabuApi.json"
+}
